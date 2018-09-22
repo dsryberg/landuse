92e110776e1a46a271e457be58ab4d57ad5874df
Severin Ryberg <sevberg129@gmail.com>
2018-09-22 22:23:42 +0200

    Basic shape drawing

--- server/serve.py
+++ server/serve.py
@@ -1,1 +1,22 @@
 # SERVER LOGIC GOES HERE
+import geokit as gk
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('base.html', name="Land Plan")
+
+@app.route('/getgeo')
+def getGeo():
+    # FILE="/home/sev/sandbox/1801_model_showcase/inputs/nrw.shp"
+    FILE=gk._test_data_["aachenShapefile.shp"]
+    # FILE=gk._test_data_["aachen_buildings.shp"]
+    vec = gk.vector.extractFeatures(FILE)
+    json = gk.vector.createGeoJson(vec)
+
+    return json
+
+if __name__ == "__main__":
+    app.run(port=3035)
